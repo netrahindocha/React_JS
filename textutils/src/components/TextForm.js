9e0a581156796f027b01a0d2509f4d3dab7ff050
netrahindocha <netrahindocha1@gmail.com>
2022-08-27 21:13:39 +0530

    Updating TextUtils 1. added toLowerCase function 2. added text summary 3. added text preview

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -8,6 +8,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = ()=>{
+        console.log("Uppercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event)=>{
         console.log("On change");
         setText(event.target.value);
@@ -17,12 +22,22 @@ export default function TextForm(props) {
     // text = "new text"; // wrong way to change the state
     // setText("new text"); // correct way to change the state
     return (
-        <div>
+        <>
+        <div className="container">
                 <h1>{props.heading}</h1>
                 <div className="form-group">
                     <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
-                    <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary my-3 mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className="btn btn-primary my-3 mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
                 </div>
         </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
